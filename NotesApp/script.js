0dace6f4c6ae4271be1bb0ed35abe8e132c07ddd
divyamhacker123 <divyam.hata1@gmail.com>
2026-07-06 23:43:49 +0530

    Added js

--- NotesApp/script.js
+++ NotesApp/script.js
@@ -1,9 +1,23 @@
 let textArea = document.getElementById('addTxt');
 let addNote = document.getElementById('add-btn');
 
+let id = localStorage.length + 1;
 addNote.addEventListener('click', function(){
     let note = textArea.value;
-    localStorage.setItem('note', note);
+    localStorage.setItem(`note${id}`, note);
+    id++;
     console.log(localStorage)
     textArea.value = '';
+})
+
+let showTxt = document.getElementById('showTxt')
+let showNote = document.getElementById('show-btn')
+showNote.addEventListener('click', ()=>{
+    let noteId = showTxt.value;
+    if (isNaN(Number(noteId))) {
+        showTxt.value = 'Please Enter valid Key'
+        return ;
+    }
+    let note = localStorage.getItem(`note${noteId}`);
+    showTxt.value = `your Searched Note is : ${note}`;
 })
